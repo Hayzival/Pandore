54332a525ed1b5a2d47410bc12d07547c84133b4
Hayzival <bryanblindorard@gmail.com>
2020-06-13 18:11:46 +0200

    Backup 13/06/2020

--- config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  TOKEN: "NzEwNTIwMjc2OTc0MDQzNTM3.Xtu-ig.aur9zzvSleLlNPmL6x3BZleLw7E",
-  DBCONNECTION: "mongodb+srv://Hayzival:brybry010603@cluster0-hr6br.mongodb.net/test?retryWrites=true&w=majority",
-  DEFAULTSETTINGS: {
-    prefix: "a%",
-    logChannel: "athena-log",
-    welcomeMessage: "Welcome {{user}}"
-  }
-}

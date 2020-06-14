b03b0c67849ad2c95f8c6827de37d9fafa56326a
Hayzival <bryanblindorard@gmail.com>
2020-06-14 17:55:19 +0200

    14/06/2020

---
+++ config-exemple.js
@@ -0,0 +1,9 @@
+module.exports = {
+  TOKEN: "TOKEN",
+  DBCONNECTION: "mongodb+srv://IDENTIFIANT:MOTDEPASSE@cluster0-hr6br.mongodb.net/test?retryWrites=true&w=majority",
+  DEFAULTSETTINGS: {
+    prefix: "a%",
+    logChannel: "pandore-log",
+    welcomeMessage: "Welcome {{user}}"
+  }
+}
--- events/member/guildMemberAdd.js
+++ events/member/guildMemberAdd.js
@@ -1,12 +1,19 @@
 const { MessageEmbed } = require("discord.js");
 
 module.exports = async (client, member) => {
+  const settings = await client.getGuild(member.guild);
+  let msg = settings.welcomeMessage;
+
+  if (msg.includes("{{user}}")) msg = msg.replace("{{user}}", member);
+
   const embed = new MessageEmbed()
     .setAuthor(`${member.displayName} (${member.id})`, member.user.displayAvatarURL())
     .setColor("#35f092")
     .setFooter("Un utilisateur a rejoint")
     .setTimestamp();
 
+  
+  client.channels.cache.get('707509037629177876').send(msg);
   client.channels.cache.get('708692148895088681').send(embed);
 
   await client.createUser({
